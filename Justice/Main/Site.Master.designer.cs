5c57ce04ddac6fd469b4ca3056b7352785b78d04
HasanShans <hasan.shans@gmail.com>
2017-11-06 17:29:26 +0400

    asdasda

--- Justice/Main/Site.Master.designer.cs
+++ Justice/Main/Site.Master.designer.cs
@@ -49,24 +49,6 @@ namespace Justice {
         protected global::System.Web.UI.WebControls.HyperLink linkMypage;
         
         /// <summary>
-        /// linkCart control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink linkCart;
-        
-        /// <summary>
-        /// lblCartCount control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblCartCount;
-        
-        /// <summary>
         /// linkLoginReg control.
         /// </summary>
         /// <remarks>
@@ -83,6 +65,24 @@ namespace Justice {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink linkNameEmail;
+        
+        /// <summary>
+        /// linkCart control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink linkCart;
+        
+        /// <summary>
+        /// lblCartCount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCartCount;
         
         /// <summary>
         /// btnSearchMaster control.
