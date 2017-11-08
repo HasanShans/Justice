494cb0acc1f5326ac348b5dee9ec9ce09466fa98
HasanShans <hasan.shans@gmail.com>
2017-11-08 16:05:18 +0400

    dasdad

--- Justice/Main/ChangePassword.aspx.cs
+++ Justice/Main/ChangePassword.aspx.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Justice.App_Code;
+﻿using Justice.App_Code;
 using System;
 using System.Collections.Generic;
 using System.Data;
